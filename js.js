54f52c9c393ab520dbd910acd260b6d0720ec819
ShynkovskyiDen <shinkovskiy.dx@gmail.com>
2023-12-12 15:10:46 +0200

    changed Length to length; added checkout

--- js.js
+++ js.js
@@ -1,20 +1,23 @@
 
-let Length = prompt('Place a length');
-let array = [];
+let length = +prompt('Введіть довжину масиву');
+const array = [];
 
-for (let i = 0; i < length; i++) {
-    let element = prompt('Place elements');
-    array.push(element);
+if (!isNaN(length)) {
+    for (let i = 0; i <= length; i++) {
+        array.push(prompt('Введіть елемент масиву' + i));
+    }
+} else {
+    alert('Некоректне значення довжини')
 }
 
-console.log(array);
+console.log('Початковий масив', array);
 
 function sort(a, b) {
     array.sort = a - b;
     return array;
 }
 
-console.log(array);
+console.log('Відсортований масив', array);
 
 array.splice(1, 3);
-console.log(array)
+console.log('Видалення елементів з 2 по 4 включно', array);
